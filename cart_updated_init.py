f47ca113d7de1355564497bcc11460c0f0c6eb1a
Vedx0609 <120775139+Vedx0609@users.noreply.github.com>
2025-01-28 16:13:02 +0530

    Added changed dao and init files

---
+++ cart_updated_init.py
@@ -0,0 +1,36 @@
+import json
+
+import products
+from cart import dao
+from products import Product
+from typing import List
+
+
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
+
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if cart_details is None:
+        return []
+    
+    ids = [j for i in cart_details for j in json.loads(i['contents'])]
+    return [products.get_product(i) for i in ids]
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
